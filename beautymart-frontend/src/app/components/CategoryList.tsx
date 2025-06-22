ae768291533da6248a95ab1acb7e28417674c9c7
J.M.N.R.D. Jayaweera <105283769+nim3xh@users.noreply.github.com>
2025-06-22 10:59:09 +0530

    Enhance CategoryList component with images and improve layout in HomePage

--- beautymart-frontend/src/app/components/CategoryList.tsx
+++ beautymart-frontend/src/app/components/CategoryList.tsx
@@ -1,7 +1,116 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
 const CategoryList = () =>{
     return(
         <div className="px-4 overflow-x-scroll scrollbar-hide">
-            CategoryList
+            <div className="flex gap-4 md:gap-8">
+                <Link
+                    href="/List/cat=test"
+                    className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
+                >
+                    <div className="relative bg-slate-100 w-full h-96">
+                        <Image
+                            src="https://images.pexels.com/photos/29524087/pexels-photo-29524087.jpeg"
+                            alt=""
+                            fill
+                            sizes="20vw"
+                            className="object-cover"
+                        />
+                    </div>
+                    <h1 className='mt-8 font-light text-xl tracking-wide'>Category Name</h1>
+                </Link>
+                <Link
+                    href="/List/cat=test"
+                    className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
+                >
+                    <div className="relative bg-slate-100 w-full h-96">
+                        <Image
+                            src="https://images.pexels.com/photos/29524087/pexels-photo-29524087.jpeg"
+                            alt=""
+                            fill
+                            sizes="20vw"
+                            className="object-cover"
+                        />
+                    </div>
+                    <h1 className='mt-8 font-light text-xl tracking-wide'>Category Name</h1>
+                </Link>
+                <Link
+                    href="/List/cat=test"
+                    className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
+                >
+                    <div className="relative bg-slate-100 w-full h-96">
+                        <Image
+                            src="https://images.pexels.com/photos/29524087/pexels-photo-29524087.jpeg"
+                            alt=""
+                            fill
+                            sizes="20vw"
+                            className="object-cover"
+                        />
+                    </div>
+                    <h1 className='mt-8 font-light text-xl tracking-wide'>Category Name</h1>
+                </Link>
+                <Link
+                    href="/List/cat=test"
+                    className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
+                >
+                    <div className="relative bg-slate-100 w-full h-96">
+                        <Image
+                            src="https://images.pexels.com/photos/29524087/pexels-photo-29524087.jpeg"
+                            alt=""
+                            fill
+                            sizes="20vw"
+                            className="object-cover"
+                        />
+                    </div>
+                    <h1 className='mt-8 font-light text-xl tracking-wide'>Category Name</h1>
+                </Link>
+                <Link
+                    href="/List/cat=test"
+                    className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
+                >
+                    <div className="relative bg-slate-100 w-full h-96">
+                        <Image
+                            src="https://images.pexels.com/photos/29524087/pexels-photo-29524087.jpeg"
+                            alt=""
+                            fill
+                            sizes="20vw"
+                            className="object-cover"
+                        />
+                    </div>
+                    <h1 className='mt-8 font-light text-xl tracking-wide'>Category Name</h1>
+                </Link>
+                <Link
+                    href="/List/cat=test"
+                    className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
+                >
+                    <div className="relative bg-slate-100 w-full h-96">
+                        <Image
+                            src="https://images.pexels.com/photos/29524087/pexels-photo-29524087.jpeg"
+                            alt=""
+                            fill
+                            sizes="20vw"
+                            className="object-cover"
+                        />
+                    </div>
+                    <h1 className='mt-8 font-light text-xl tracking-wide'>Category Name</h1>
+                </Link>
+                <Link
+                    href="/List/cat=test"
+                    className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
+                >
+                    <div className="relative bg-slate-100 w-full h-96">
+                        <Image
+                            src="https://images.pexels.com/photos/29524087/pexels-photo-29524087.jpeg"
+                            alt=""
+                            fill
+                            sizes="20vw"
+                            className="object-cover"
+                        />
+                    </div>
+                    <h1 className='mt-8 font-light text-xl tracking-wide'>Category Name</h1>
+                </Link>
+            </div>
         </div>
     )
 }
--- beautymart-frontend/src/app/page.tsx
+++ beautymart-frontend/src/app/page.tsx
@@ -6,16 +6,16 @@ const HomePage =() =>{
   return (
     <div className="">
       <Slider />
-      <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
-        <h1>Featured Products</h1>
+      <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+        <h1 className="text-2xl">Featured Products</h1>
         <ProductList />
       </div>
-      <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
-        <h1>Categories</h1>
+      <div className="mt-24">
+        <h1 className="text-2xl px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 mb-12">Categories</h1>
         <CategoryList />
       </div>
       <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
-        <h1>New Products</h1>
+        <h1 className="text-2xl">New Products</h1>
         <ProductList />
       </div>
     </div>
