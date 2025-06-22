45ad073b7fdea4ba96855493fc3eb0909a68c23a
J.M.N.R.D. Jayaweera <105283769+nim3xh@users.noreply.github.com>
2025-06-22 10:43:21 +0530

    Add CategoryList component and integrate into HomePage

---
+++ beautymart-frontend/src/app/components/CategoryList.tsx
@@ -0,0 +1,9 @@
+const CategoryList = () =>{
+    return(
+        <div className="px-4 overflow-x-scroll scrollbar-hide">
+            CategoryList
+        </div>
+    )
+}
+
+export default CategoryList;
--- beautymart-frontend/src/app/page.tsx
+++ beautymart-frontend/src/app/page.tsx
@@ -1,5 +1,6 @@
 import Slider from "./components/Slider";
 import ProductList from "./components/ProductList";
+import CategoryList from "./components/CategoryList";
 
 const HomePage =() =>{
   return (
@@ -7,6 +8,14 @@ const HomePage =() =>{
       <Slider />
       <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
         <h1>Featured Products</h1>
+        <ProductList />
+      </div>
+      <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+        <h1>Categories</h1>
+        <CategoryList />
+      </div>
+      <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+        <h1>New Products</h1>
         <ProductList />
       </div>
     </div>
